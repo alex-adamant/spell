ddfd99eab33b2cf8291a3bfba1120b6054c504a3
Alexandr Adamant <alexandradamant@gmail.com>
2020-05-24 16:50:05 +0200

    add specific host

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
 
-fastify.listen(9000, 'https://13.49.77.84/', (err, address) => {
+fastify.listen(9000, '13.49.77.84', (err, address) => {
   if (err) throw err
   console.log(`Server is listening on ${address}`)
 })
