8d61cfa806ed7c00a4a8f38fa5f4624e39c0a526
Alexandr Adamant <alexandradamant@gmail.com>
2020-05-24 17:06:57 +0200

    add express

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express()
 app.get('/', function (res, req) {
   res.send('Hello world(test2)!\n')
 })
-var port = 80
+var port = 8080
 app.listen(port)
 console.log('Listening on port', port)
