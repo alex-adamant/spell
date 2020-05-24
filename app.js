ece544df96db973a64097f5a5cc5a7b41d9a45fb
Alexandr Adamant <alexandradamant@gmail.com>
2020-05-24 17:07:40 +0200

    add express

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 app.get('/', function (res, req) {
-  res.send('Hello world(test2)!\n')
+  req.send('Hello world(test2)!\n')
 })
 var port = 8080
 app.listen(port)
